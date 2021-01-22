9d9694160f70e77f9ff47bf3dabdea47cf9fbc23
Santhoshmathavan K <45665384+santymaddy199@users.noreply.github.com>
2021-01-22 13:05:39 +0530

    day-3

---
+++ JavaScript/Intermediate/objects.js
@@ -0,0 +1,113 @@
+// const person = {
+//     firstName: 'Asabeneh',
+//     lastName: 'Yetayeh',
+//     age: 250,
+//     country: 'Finland',
+//     city: 'Helsinki',
+//     skills: [
+//       'HTML',
+//       'CSS',
+//       'JavaScript',
+//       'React',
+//       'Node',
+//       'MongoDB',
+//       'Python',
+//       'D3.js'
+//     ],
+//     getFullName: function() {
+//       return `${this.firstName} ${this.lastName}`
+//     }
+//   }
+//   person.nationality = 'Ethiopian'
+//   person.country = 'Finland'
+//   person.title = 'teacher'
+//   person.skills.push('Meteor')
+//   person.skills.push('SasS')
+//   person.isMarried = true
+  
+//   person.getPersonInfo = function() {
+//     let skillsWithoutLastSkill = this.skills
+//       .splice(0, this.skills.length - 1)
+//       .join(', ')
+//     let lastSkill = this.skills.splice(this.skills.length - 1)[0]
+//     let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
+//     let fullName = this.getFullName()
+//     let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`
+//     return statement
+//   }
+
+//   console.log()
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+
+    // changeName:function(name){
+    //     this.Paul.email = name
+    // }
+}
+
+//---------------------------------Count of persons with logged and points----------------------
+// let count=0
+// for(const ele in users){
+//     if(users[ele].isLoggedIn==true && users[ele].points>=50){
+//         count++
+//     }
+// }
+// console.log(count)
+
+//---------------------------------Find the person with maximum skills-------------------------
+// let arr=[]
+// for(const ele in users){
+//     const values = Object.values(users[ele].skills)
+//     arr.push(values.length)
+// }
+// console.log(Math.max(...arr))
+
+console.log(users)
