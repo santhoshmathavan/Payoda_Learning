7eccf58815ad21da42ba98dc21c1576220310b50
Santhoshmathavan K <45665384+santymaddy199@users.noreply.github.com>
2021-01-22 16:12:43 +0530

    Day-3

---
+++ JavaScript/Intermediate/higherfunctions.js
@@ -0,0 +1,24 @@
+// const callback = (n) =>{
+//     return n**2
+// }
+
+// function cube(callback,n){
+//     return callback(n) * n;
+// }
+
+// console.log(cube(callback,3))
+
+// arr = [1,2,3,4,5]
+
+// arr.forEach(num=>console.log(num))
+
+// const products = [
+//     {product:'Wm',price:50},
+//     {product:'Cum',price:75},
+//     {product:'Lemon',price:64},
+//     {product:'Juice',price:725},
+// ]
+
+// for(const pro of products){
+//     console.log(`The product is ${pro.product} and price is${pro.price}`)
+// }
