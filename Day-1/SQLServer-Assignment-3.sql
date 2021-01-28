a850715a7af93796bd50089a7c78e65e68db3cc3
Santhoshmathavan K <45665384+santhoshmathavan@users.noreply.github.com>
2021-01-28 07:59:26 +0530

    Add files via upload

---
+++ Day-1/SQLServer-Assignment-3.sql
@@ -0,0 +1,9 @@
+INSERT INTO customers(customer_id,first_name,last_name,phone,email,street,city,state,zip_code)
+VALUES
+(1,'ram','Kumar','9076842109','ram@gmail.com','ram Street','Syndey','Aus','47346'),
+(2,'Kunal','Kumar','9076842109','kunal@gmail.com','ram Street','Syndey','Aus','47346'),
+(3,'Ramya','Varshini','9876674210','ramya@gmail.com','Naidu Street','Trichy','TN','97546'),
+(4,'Rudhra','Kumari','9076856789','rudhra@gmail.com','Dammal Street','Ladak','Ladak','00004'),
+(5,'Vishu','Vardhan','9875562210','vishnu@gmail.com','Pappu Street','Trichy','TN','86544')
+
+SELECT * FROM customers
