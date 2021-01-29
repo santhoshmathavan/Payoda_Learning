56745e63416777b0b71c35abc09bce001c983d32
Santhoshmathavan K <45665384+santhoshmathavan@users.noreply.github.com>
2021-01-29 10:28:11 +0530

    Add files via upload

---
+++ Day-2/SQLServerQueries-Assignment-2-Day2.sql
@@ -0,0 +1,32 @@
+use DB
+
+select * from train
+
+select
+sum(case when gender='Male' then 1 else 0 end) as Total_Number_Of_Male_Employee,
+sum(case when gender='Female' then 1 else 0 end) as Total_Number_Of_Female_Employee
+from train
+group by response
+having response=1;
+
+select
+sum(case when gender='Male' then 1 else 0 end) as Total_Number_Of_Male_Employee,
+sum(case when gender='Female' then 1 else 0 end) as Total_Number_Of_Female_Employee,
+Response
+from train
+group by response
+having response=0 or response=1;
+
+select avg(age) as Average_Age_of_Male
+from train
+where gender='Male' and response=1;
+
+select max(Annual_Premium) as Maximum_Annual_Premium
+from train
+where gender='Female' and Response=1;
+
+select * from train
+
+select count(Vehicle_Age) as Vechile_Age,Vehicle_Age
+from train
+group by Vehicle_Age
